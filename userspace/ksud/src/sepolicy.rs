59d8ad1cdd74e9c1301fa08486fcee460e30a248
Rissu <90097027+rsuntk@users.noreply.github.com>
2025-03-05 22:02:34 +0700

    ksud: Address warnings

--- userspace/ksud/src/sepolicy.rs
+++ userspace/ksud/src/sepolicy.rs
@@ -1,26 +1,14 @@
-use anyhow::{bail, Result};
-use derive_new::new;
-use nom::{
-    branch::alt,
-    bytes::complete::{tag, take_while, take_while1, take_while_m_n},
-    character::{
-        complete::{space0, space1},
-        is_alphanumeric,
-    },
-    combinator::map,
-    sequence::Tuple,
-    IResult, Parser,
-};
-use std::{ffi, path::Path, vec};
+use anyhow::Result;
+use std::path::Path;
 
 pub fn live_patch(policy: &str) -> Result<()> {
     Ok(())
 }
 
-pub fn apply_file<P: AsRef<Path>>(path: P) -> Result<()> {
+pub fn apply_file<P: AsRef<Path>>(_path: P) -> Result<()> {
     Ok(())
 }
 
-pub fn check_rule(policy: &str) -> Result<()> {
+pub fn check_rule(_policy: &str) -> Result<()> {
     Ok(())
 }
